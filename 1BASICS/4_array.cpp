cf8e4558999b2181d5983b04f5bad9f0a0031bd3
kingadarsh <adarsh261201@gmail.com>
2023-10-23 18:12:18 +0530

    Dynamic array

--- 1BASICS/4_array.cpp
+++ 1BASICS/4_array.cpp
@@ -1,17 +1,44 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//    int n;
+//    cin>>n;
+//
+//    int array[n];
+//
+//    for(int i=0;i<n;i++){
+//        cin>>array[i];
+//    }
+//    for(int i=0;i<n;i++){
+//        cout<<array[i]<<" ";
+//    }
+//    return 0;
+//}
+//
+
 #include<iostream>
+#include<vector>
 using namespace std;
-
-int main(){
+int main( ){
     int n;
+    cout<<"give the number of elements to be in the array : ";
     cin>>n;
-
-    int array[n];
-
+    
+    
+    vector<int> arr(n);
+    cout<<"Put the numbers into the array"<<endl;
     for(int i=0;i<n;i++){
-        cin>>array[i];
+        cin>>arr[i];
     }
-    for(int i=0;i<n;i++){
-        cout<<array[i]<<" ";
+    cout<<"Your elements are : ";
+    
+    for(int j = 0 ; j<n ; j++){
+        cout<<arr[j]<<" ";
     }
-    return 0;
+    
 }
+
+
+/////////////////////////////  USER DESIGNED STATIC ARRAY IS A BAD PRACTICE SINCE STATIC ARRAY IS CONTIGUOUS AND UTILIZES MEMORY IN THE REAL TIME ///////////////////////////////////
+ 
